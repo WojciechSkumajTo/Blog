786f340ae6ef0ea970f61452c29a8488ddb2cc1c
WojciechSkumajTo <h4ndrish@gmail.com>
2023-04-03 19:48:33 +0200

    Add render template method

--- WebApp/flaskBlog.py
+++ WebApp/flaskBlog.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-"Data base"
 
 posts = [
     {
@@ -27,16 +26,14 @@ posts = [
 ]
 
 
-
 @app.route('/')
 def hello():
-    return render_template('home.html', posts=posts)
-
+    return render_template('home.html', posts=posts, title='IDK')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About!!!')
 
 if __name__ == '__main__':
     app.run(debug=True)
